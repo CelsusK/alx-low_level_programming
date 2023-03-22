6f95943502c5970e0d8e8f54072a016822e1730b
CelsusK <kiragucassian@gmail.com>
2023-03-22 13:09:36 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/main_0.c
+++ 0x02-functions_nested_loops/main_0.c
@@ -1,1 +1,18 @@
-100-times_table.c
+#include "main.h"
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_times_table(3);
+    _putchar('\n');
+    print_times_table(5);
+    _putchar('\n');
+    print_times_table(98);
+    _putchar('\n');
+    print_times_table(12);  
+    return (0);
+}
