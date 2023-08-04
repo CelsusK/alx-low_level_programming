3498c4c3b220e2afe250e06ce31945f531d7626a
CelsusK <kiragucassia@gmail.com>
2023-08-04 10:27:38 -0700

    Update

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_binary - Prints the binary representation of a number.
+ *
+ * @n: The number to print.
+ *
+ * Return: Void
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int mask;
+	int flag = 0;
+
+	if (!n)
+	{
+		_putchar('0');
+		return;
+	}
+
+	mask = 1UL << (sizeof(unsigned long int) * 8 - 1);
+
+	while (mask > 0)
+	{
+		if ((n & mask))
+		{
+			_putchar('1');
+			flag = 1;
+		}
+		else if (flag == 1 || mask == 1)
+			_putchar('0');
+		mask >>= 1;
+	}
+}
---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,24 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#define PASSWORD_LENGTH 8
+
+/* Header Files */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <time.h>
+
+/* prototype functions */
+unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
+int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
+int get_endianness(void);
+int _putchar(int c);
+void generatePassword(char *password);
+
+#endif /* MAIN_H */
