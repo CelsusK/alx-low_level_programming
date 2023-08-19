99bc7433cc35e085dc5cbcbc7d874e95f3ac9a26
CelsusK <kiragucassian@gmail.com>
2023-08-18 19:48:41 -0700

    Update

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -11,13 +11,13 @@
 int sum_dlistint(dlistint_t *head)
 {
 
-        int sum = 0;
-        dlistint_t *i;
+	int sum = 0;
+	dlistint_t *i;
 
-        for (i = head; i != NULL; i = i->next)
-        {
-                sum += i->n;
-        }
+	for (i = head; i != NULL; i = i->next)
+	{
+		sum += i->n;
+	}
 
-        return (sum);
+	return (sum);
 }
